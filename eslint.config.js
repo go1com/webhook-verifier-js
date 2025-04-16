89995f13ecb9f740c3922086d8f20ff1ac7db618
An Hoang <hoangthienan@gmail.com>
2025-04-16 22:25:26 +0700

    chore(tsconfig): include test files to enable ESLint type-aware linting + lint but not compile tests into dist/

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ module.exports = [
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
       },
     },
     plugins: {
